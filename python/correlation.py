b7c66883b2109194ebfacaeef0b3a6581d5fd19d
Weslley Lioba Caldas <weslleylc@gmail.com>
2018-11-30 22:30:28 -0200

    adcionando correlação

--- python/correlation.py
+++ python/correlation.py
@@ -17,27 +17,23 @@ types = pd.read_csv(data_folder/'pokemon_list.csv', ',', engine='python')
 if types.columns[0] == 'Unnamed: 0.1':
     types = types.drop([types.columns[0]],axis=1)
 
-
-dfFull=df.merge(types[['Pokemon No.', 'Type 1', 'Type 2']], left_on='pokemonId', right_on='Pokemon No.', how='outer')
-
-columns=pd.get_dummies(dfFull['Type 1'])
-
 terrainTypesDict = {0: "Water", 1: "forest", 2: "forest", 3: "forest", 4: "forest", 5: "forest", 6: "shrublands", 7: "shrublands", 8: "savannas", 9: "savannas", 10: "grasslands", 11: "wetlands", 12: "croplands", 13: "urban",14: "cropland", 15: "ice", 16: "barren"}
 df.replace({'terrainType': terrainTypesDict},  inplace=True)
-terrainType=pd.get_dummies(df['terrainType'])
-columns=columns.astype(bool)
-dfFull=dfFull[['pokemonId', 'latitude', 'longitude',
-       'terrainType', 'closeToWater', 'city', 'continent', 'weather',
-       'temperature', 'windSpeed', 'windBearing', 'pressure',
-       'population_density', 'urban', 'suburban', 'midurban', 'rural']]
 
-dfFull[columns.columns]=columns
+dfFull=df.merge(types[['Pokemon No.', 'Type 1']], left_on='pokemonId', right_on='Pokemon No.', how='outer')
+
+typesPokemon=pd.get_dummies(dfFull['Type 1'])
+typesPokemon=typesPokemon.astype(bool)
+
+terrainType=pd.get_dummies(dfFull['terrainType'])
+terrainType=terrainType.astype(bool)
+
+dfFull.drop(['Type 1','terrainType', 'Pokemon No.','continent'],axis=1,inplace=True)
+
+dfFull[typesPokemon.columns]=typesPokemon
 dfFull[terrainType.columns]=terrainType
-dfFull=dfFull[~dfFull.isnull().any(axis=1)]
-dfFull['closeToWater']=dfFull['closeToWater'].astype(bool)
-dfFull['urban']=dfFull['urban'].astype(bool)
-dfFull['suburban']=dfFull['suburban'].astype(bool)
-dfFull['midurban']=dfFull['midurban'].astype(bool)
-dfFull['rural']=dfFull['rural'].astype(bool)
 
+dfFull=dfFull[~dfFull.isnull().any(axis=1)]
+dfFull[['closeToWater','urban','suburban','midurban','rural']]=dfFull[['closeToWater','urban','suburban','midurban','rural']].astype(bool)
+dfFull['weather']=dfFull['weather'].astype('str')
 corr=dfFull.corr()
